34783471674e4ef5768036882a7d6aff0bd6d0ee
523brain <523brain@gmail.com>
2013-06-11 21:15:23 +0200

    Berechnung für 24Bit geht andere folgt in kürze+validations fehler behoben eingabe auf decimalnumber bei TextEdit Feldern eingestellt

--- src/com/example/netman/Main.java
+++ src/com/example/netman/Main.java
@@ -1,5 +1,7 @@
 package com.example.netman;
 
+import java.text.DecimalFormat;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -38,7 +40,7 @@ public class Main extends Activity {
     	TextView tv_max_client = (TextView)findViewById(R.id.tv_max_client);
     	TextView tv_first_client = (TextView)findViewById(R.id.tv_first_client);
     	TextView tv_last_client = (TextView)findViewById(R.id.tv_last_client);
-    	TextView tv_boradcast = (TextView)findViewById(R.id.tv_boradcast);
+    	TextView tv_broadcast = (TextView)findViewById(R.id.tv_boradcast);
     	TextView tvh_max_client = (TextView)findViewById(R.id.tvh_max_client);
     	TextView tvh_first_client = (TextView)findViewById(R.id.tvh_first_client);
     	TextView tvh_last_client = (TextView)findViewById(R.id.tvh_last_client);
@@ -82,6 +84,27 @@ public class Main extends Activity {
     	}
     	//End Validation Check
     	
+    	String bIP = "";
+    	String [] subs = ip.getText().toString().split ("\\.");
+    	for (String s : subs){
+    		if(Integer.toBinaryString(Integer.parseInt(s)).length()<8){
+    			for (int i=0;i<(8-Integer.toBinaryString(Integer.parseInt(s)).length());i++){
+    				bIP = bIP + "0";
+    			}
+    			bIP = bIP + Integer.toBinaryString(Integer.parseInt(s));
+    		} else {
+    			bIP = bIP + Integer.toBinaryString(Integer.parseInt(s));
+    		}
+    		 		
+    	}
+    	String netbin = bIP.substring(0,Integer.parseInt(bit.getText().toString()));
+        netbin = splitBinary(netbin,".");
+        DecimalFormat x = new DecimalFormat ("#");
+        tv_max_client.setText(x.format(Math.pow(2,(32-Integer.parseInt(bit.getText().toString())))));
+        tv_first_client.setText(BinTailToDec(netbin+"1"));
+        tv_last_client.setText(BinTailToDec(netbin+"11111110"));
+    	tv_broadcast.setText(BinTailToDec(netbin+"11111111"));
+    	
     	//Set Result Data Visible
     	TextViewMaxClient.setVisibility(1);
     	textViewFirstClient.setVisibility(1);
@@ -90,7 +113,7 @@ public class Main extends Activity {
     	tv_max_client.setVisibility(1);
     	tv_first_client.setVisibility(1);
     	tv_last_client.setVisibility(1);
-    	tv_boradcast.setVisibility(1);
+    	tv_broadcast.setVisibility(1);
     	tvh_max_client.setVisibility(1);
     	tvh_first_client.setVisibility(1);
     	tvh_last_client.setVisibility(1);
@@ -98,10 +121,43 @@ public class Main extends Activity {
     	return true;
     }
     
+    public String splitBinary(String input, String sym){
+    	String all = "";
+    	String tmp = input;
+    	if (tmp.length()<=8){
+    		all = tmp.substring(0,8)+".";
+    	}
+    	if (tmp.length()>8 && tmp.length()<=16){
+    		all = tmp.substring(0,8)+"."+tmp.substring(8,16)+".";
+    	}
+    	if (tmp.length()>16){
+    		all = tmp.substring(0,8)+"."+tmp.substring(8,16)+"."+tmp.substring(16, 24)+".";
+    	}
+    	return all;
+    }
+    
+    public String BinTailToDec(String tail)
+    {
+        String all ="";
+        int j = 0;
+        String [] parts = tail.split ("\\.");
+        for (String s : parts){
+        	j++;
+        	if (j>3){
+        		all = all + String.valueOf(Integer.parseInt(s,2));
+        	} else {
+        		all = all + String.valueOf(Integer.parseInt(s,2))+".";
+        	}
+        	//Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+        	//Toast.makeText(getApplicationContext(), String.valueOf(Integer.parseInt(s,2)), Toast.LENGTH_LONG).show();
+        }
+        return all;
+    }
+    
     public static boolean checkIp (String sip)
     {
         String [] parts = sip.split ("\\.");
-        if ( parts.length != 3 ){
+        if ( parts.length != 4 ){
         	return false;
         }
         for (String s : parts){
